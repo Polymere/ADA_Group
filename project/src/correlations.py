c3d2ac44b4c2d61a2dc7ba39f4c26631f4c7b544
marcadamsge <marc_adams@hotmail.fr>
2017-12-13 18:11:16 +0100

    global correlation implemented

--- project/src/correlations.py
+++ project/src/correlations.py
@@ -1,64 +1,50 @@
-from pyspark import RDD
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 from pyspark import SparkContext
+from cluster_utils import get_rdd
+import analysis_functions
+from pyspark.mllib.stat import Statistics
+import pickle
 
-from analysis_functions import is_numeric
-
-
-def scatter_plot_with_song_hotness(rdd, metadata_songs_rdd, number_of_points=500):
-    """
-    :type rdd: RDD
-    :type metadata_songs_rdd: RDD
-    """
-    hotness = metadata_songs_rdd.map(lambda x: (x[0], x[1]['song_hotttnesss'][0])).filter(lambda x: is_numeric(x[1]))
-    joined_rdds = rdd.join(hotness)
-    points = joined_rdds.sample(False, float(number_of_points)/float(joined_rdds.count())).map(lambda x: x[1]).collect()
-    x = [i[0] for i in points]
-    y = [i[1] for i in points]
-    plt.plot(x, y, '.')
-    plt.show()
+# OUT_PATH = '/buffer/mrp_buffer/histograms/'
+OUT_PATH = 'test/'
 
 
-def scatter_plot_with_song_hotness_and_year(rdd, metadata_songs_rdd, music_brainz_rdd, number_of_points=500):
-    hotness = metadata_songs_rdd.map(lambda x: (x[0], x[1]['song_hotttnesss'][0])).filter(lambda x: is_numeric(x[1]))
-    year = music_brainz_rdd.map(lambda x: (x[0], x[1]['year'][0])).filter(lambda x: not (int(x[1]) == 0))
-    joined_rdds = rdd.join(hotness).join(year)
-    points = joined_rdds.sample(False, float(number_of_points) / float(joined_rdds.count())).map(
-        lambda x: x[1]).collect()
-    x = [i[0][0] for i in points]
-    y = [i[0][1] for i in points]
-    z = [i[1] for i in points]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z)
-    plt.show()
+def correlation_preparation(rdd, field_name):
+    return rdd.map(lambda x: (x[0], x[1][field_name][0])) \
+        .filter(lambda x: analysis_functions.is_numeric(x[1])) \
+        .filter(lambda x: float(x[1]) != 0.0)
 
 
-def correlation_artist_hotness(sc):
-    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
-    artist_hotness = metadata_songs_rdd.map(
-        lambda x: (x[0], x[1]['song_hotttnesss'][0])
-    ).filter(lambda x: is_numeric(x[1]))
-    scatter_plot_with_song_hotness(artist_hotness, metadata_songs_rdd)
+def calculate_correlation(rdd1, rdd2, method="pearson"):
+    # we join to keep only the element we have in common
+    rdd = rdd1.join(rdd2)
+    return Statistics.corr(rdd.map(lambda x: x[1][0]), rdd.map(lambda x: x[1][1]), method=method)
 
 
-def correlation_tempo(sc):
-    analysis = sc.pickleFile(r'../data/analysis-songs')
-    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
-    tempo = analysis.map(lambda x: (x[0], x[1]['tempo'][0])).filter(lambda x: is_numeric(x[1]))
-    scatter_plot_with_song_hotness(tempo, metadata_songs_rdd)
+if __name__ == "__main__":
+    sc = SparkContext(appName='CorrelationOfData')
+    DATA_DIR = 'data/'
+    music_brainz_rdd = sc.pickleFile(DATA_DIR + 'musicbrainz-songs')
+    analysis_songs_rdd = sc.pickleFile(DATA_DIR + 'analysis-songs')
+    metadata_songs_rdd = sc.pickleFile(DATA_DIR + 'metadata-songs')
+    # music_brainz_rdd = get_rdd('musicbrainz-songs', sc)
+    # analysis_songs_rdd = get_rdd('analysis-songs', sc)
+    # metadata_songs_rdd = get_rdd('metadata-songs', sc)
+    data = {
+        'hotness': correlation_preparation(metadata_songs_rdd, 'song_hotttnesss'),
+        'duration': correlation_preparation(analysis_songs_rdd, 'duration'),
+        'key': correlation_preparation(analysis_songs_rdd, 'key'),
+        'loudness': correlation_preparation(analysis_songs_rdd, 'loudness'),
+        'tempo': correlation_preparation(analysis_songs_rdd, 'tempo'),
+        'time_signature': correlation_preparation(analysis_songs_rdd, 'time_signature'),
+        'latitude': correlation_preparation(metadata_songs_rdd, 'artist_latitude'),
+        'longitude': correlation_preparation(metadata_songs_rdd, 'artist_longitude'),
+        'year': correlation_preparation(music_brainz_rdd, 'year'),
+    }
 
+    correlations = {}
+    for i in ['duration', 'key', 'loudness', 'tempo', 'time_signature', 'latitude', 'longitude', 'year']:
+        correlations[i] = calculate_correlation(data['hotness'], data[i])
 
-def correlation_tempo_and_year(sc):
-    music_brainz_rdd = sc.pickleFile(r'../data/musicbrainz-songs')
-    analysis_songs_rdd = sc.pickleFile(r'../data/analysis-songs')
-    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
-    tempo = analysis_songs_rdd.map(lambda x: (x[0], x[1]['tempo'][0])).filter(lambda x: is_numeric(x[1]))
-    scatter_plot_with_song_hotness_and_year(tempo, metadata_songs_rdd, music_brainz_rdd)
-
-
-if __name__ == '__main__':
-    sc = SparkContext(appName='testCorrelation')
-    correlation_tempo_and_year(sc)
-    sc.stop()
+    with open(OUT_PATH + 'correlations-global', 'wb') as out_file:
+        pickle.dump(correlations, out_file)
+       
--- project/src/graphing_functions.py
+++ project/src/graphing_functions.py
@@ -4,7 +4,7 @@ import numpy as np
 from pyspark import SparkContext
 import matplotlib.pyplot as pyplot
 
-from analysis_functions import numeric_array_or_matrix_histogram
+from analysis_functions import numeric_array_or_matrix_histogram, is_numeric
 
 
 def print_histogram(histogram, number_of_labels=10, title="", figsize=(8, 6), dpi=80):
@@ -23,6 +23,58 @@ def print_histogram(histogram, number_of_labels=10, title="", figsize=(8, 6), dp
 
 def format_labels(labels):
     return [format(i, '.2e') for i in labels]
+
+
+def scatter_plot_with_song_hotness(rdd, metadata_songs_rdd, number_of_points=500):
+    """
+    :type rdd: RDD
+    :type metadata_songs_rdd: RDD
+    """
+    hotness = metadata_songs_rdd.map(lambda x: (x[0], x[1]['song_hotttnesss'][0])).filter(lambda x: is_numeric(x[1]))
+    joined_rdds = rdd.join(hotness)
+    points = joined_rdds.sample(False, float(number_of_points)/float(joined_rdds.count())).map(lambda x: x[1]).collect()
+    x = [i[0] for i in points]
+    y = [i[1] for i in points]
+    plt.plot(x, y, '.')
+    plt.show()
+
+
+def scatter_plot_with_song_hotness_and_year(rdd, metadata_songs_rdd, music_brainz_rdd, number_of_points=500):
+    hotness = metadata_songs_rdd.map(lambda x: (x[0], x[1]['song_hotttnesss'][0])).filter(lambda x: is_numeric(x[1]))
+    year = music_brainz_rdd.map(lambda x: (x[0], x[1]['year'][0])).filter(lambda x: not (int(x[1]) == 0))
+    joined_rdds = rdd.join(hotness).join(year)
+    points = joined_rdds.sample(False, float(number_of_points) / float(joined_rdds.count())).map(
+        lambda x: x[1]).collect()
+    x = [i[0][0] for i in points]
+    y = [i[0][1] for i in points]
+    z = [i[1] for i in points]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
+    plt.show()
+
+
+def correlation_artist_hotness(sc):
+    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
+    artist_hotness = metadata_songs_rdd.map(
+        lambda x: (x[0], x[1]['song_hotttnesss'][0])
+    ).filter(lambda x: is_numeric(x[1]))
+    scatter_plot_with_song_hotness(artist_hotness, metadata_songs_rdd)
+
+
+def correlation_tempo(sc):
+    analysis = sc.pickleFile(r'../data/analysis-songs')
+    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
+    tempo = analysis.map(lambda x: (x[0], x[1]['tempo'][0])).filter(lambda x: is_numeric(x[1]))
+    scatter_plot_with_song_hotness(tempo, metadata_songs_rdd)
+
+
+def correlation_tempo_and_year(sc):
+    music_brainz_rdd = sc.pickleFile(r'../data/musicbrainz-songs')
+    analysis_songs_rdd = sc.pickleFile(r'../data/analysis-songs')
+    metadata_songs_rdd = sc.pickleFile(r'../data/metadata-songs')
+    tempo = analysis_songs_rdd.map(lambda x: (x[0], x[1]['tempo'][0])).filter(lambda x: is_numeric(x[1]))
+    scatter_plot_with_song_hotness_and_year(tempo, metadata_songs_rdd, music_brainz_rdd)
 
 
 if __name__ == "__main__":
