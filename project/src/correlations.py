e02bc2450055b4e9cfd813acfe4862838e071d23
marc <marc_adams@hotmail.fr>
2017-12-13 19:39:35 +0100

    Correlations per year implemented

--- project/src/correlations.py
+++ project/src/correlations.py
@@ -4,8 +4,8 @@ import analysis_functions
 from pyspark.mllib.stat import Statistics
 import pickle
 
-# OUT_PATH = '/buffer/mrp_buffer/histograms/'
-OUT_PATH = 'test/'
+OUT_PATH = '/buffer/mrp_buffer/correlations/'
+# OUT_PATH = 'test/'
 
 
 def correlation_preparation(rdd, field_name):
@@ -17,18 +17,21 @@ def correlation_preparation(rdd, field_name):
 def calculate_correlation(rdd1, rdd2, method="pearson"):
     # we join to keep only the element we have in common
     rdd = rdd1.join(rdd2)
-    return Statistics.corr(rdd.map(lambda x: x[1][0]), rdd.map(lambda x: x[1][1]), method=method)
+    return {
+        'corr': Statistics.corr(rdd.map(lambda x: x[1][0]), rdd.map(lambda x: x[1][1]), method=method),
+        'nb_elements': rdd.count()
+    }
 
 
 if __name__ == "__main__":
     sc = SparkContext(appName='CorrelationOfData')
-    DATA_DIR = 'data/'
-    music_brainz_rdd = sc.pickleFile(DATA_DIR + 'musicbrainz-songs')
-    analysis_songs_rdd = sc.pickleFile(DATA_DIR + 'analysis-songs')
-    metadata_songs_rdd = sc.pickleFile(DATA_DIR + 'metadata-songs')
-    # music_brainz_rdd = get_rdd('musicbrainz-songs', sc)
-    # analysis_songs_rdd = get_rdd('analysis-songs', sc)
-    # metadata_songs_rdd = get_rdd('metadata-songs', sc)
+    # DATA_DIR = 'data/'
+    # music_brainz_rdd = sc.pickleFile(DATA_DIR + 'musicbrainz-songs')
+    # analysis_songs_rdd = sc.pickleFile(DATA_DIR + 'analysis-songs')
+    # metadata_songs_rdd = sc.pickleFile(DATA_DIR + 'metadata-songs')
+    music_brainz_rdd = get_rdd('musicbrainz-songs', sc)
+    analysis_songs_rdd = get_rdd('analysis-songs', sc)
+    metadata_songs_rdd = get_rdd('metadata-songs', sc)
     data = {
         'hotness': correlation_preparation(metadata_songs_rdd, 'song_hotttnesss'),
         'duration': correlation_preparation(analysis_songs_rdd, 'duration'),
@@ -41,10 +44,21 @@ if __name__ == "__main__":
         'year': correlation_preparation(music_brainz_rdd, 'year'),
     }
 
-    correlations = {}
+    correlations = dict()
     for i in ['duration', 'key', 'loudness', 'tempo', 'time_signature', 'latitude', 'longitude', 'year']:
         correlations[i] = calculate_correlation(data['hotness'], data[i])
 
     with open(OUT_PATH + 'correlations-global', 'wb') as out_file:
         pickle.dump(correlations, out_file)
-       
+
+    correlations_per_year = dict()
+    for year in range(1922, 2018):
+        correlations_per_year[year] = dict()
+        y = data['year'].filter(lambda x: x[1] == year)
+        for i in ['duration', 'key', 'loudness', 'tempo', 'time_signature', 'latitude', 'longitude']:
+            # filtering tuples of that year
+            rdd = y.join(data[i]).map(lambda x: (x[0], x[1][1]))
+            correlations_per_year[year][i] = calculate_correlation(data['hotness'], rdd)
+
+    with open(OUT_PATH + 'correlations-per-year', 'wb') as out_file:
+        pickle.dump(correlations, out_file)
