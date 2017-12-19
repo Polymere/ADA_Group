3687334ca0be7ea865f67bfa24c0034d3b78adee
Polymere <paul.prevel@epfl.ch>
2017-12-19 21:28:08 +0100

    update feature_hit

--- project/src/feature_hit.py
+++ project/src/feature_hit.py
@@ -1,5 +1,4 @@
-import analysis_terms as an_tag
-
+from analysis_terms import map_rdd_tags,count_tags_weight
 from pyspark import SparkContext
 from pyspark import RDD
 from operator import add
@@ -7,7 +6,6 @@ from cluster_utils import get_rdd
 
 if __name__ == '__main__':
     sc = SparkContext()
-    lst, rdd = get_vector_terms(sc)
     main_rdd=an_tag.map_rdd_tags(sc)
     hit_rdd=sc.pickleFile('hdfs:///user/prevel/new_pickle_hit')
     filtered_rdd=main_rdd.filter(lambda x:[x[0] in hit_rdd.keys]
